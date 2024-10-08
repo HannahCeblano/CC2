3e870ba6a82e552957e4a2b0b3a72eb9ef144e87
HannahCeblano <ceblanoh@gmail.com>
2024-10-08 21:20:23 +0800

    Add files via upload

---
+++ Lab2.java
@@ -0,0 +1,25 @@
+//Hannah Marielle O. Ceblano
+//CICTS 1N-A
+//Laboratory Challenge No.2
+
+public class Lab2 {
+	public static void main(String[] args) {
+
+
+		//Variable with set values
+		double length = 5;
+		double width = 4;
+		//formula of Area and Perimeter as Variable
+		double area = length * width;
+		double perimeter = 2 * area;
+
+		//printing the Area and Perimeter String+Variable+String
+		System.out.println("The area is:  "+area+"m");
+		System.out.println("The perimeter is: "+perimeter+"m");
+		System.out.println("The width is:  "+width+"m");
+		System.out.println("The length is:  "+length+"m");
+
+
+	}
+
+}
