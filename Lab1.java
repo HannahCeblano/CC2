5dd48b9e74541ab70ac43ec436409c215837359a
HannahCeblano <ceblanoh@gmail.com>
2024-10-08 21:14:53 +0800

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,25 @@
+// First Programming Activity
+// Hannah Marielle O. Ceblano
+// September 3, 2024
+//Laboratory No.1
+
+public class Lab1 {
+	public static void main(String[] args) {
+
+		//Prints the store name and the address
+		System.out.println("Yuki's Store\nOutlook Drive, Baguio City\n");
+
+		//Prints the item name,quantities and price
+		System.out.println("Item name\tQuantities\t\t\tPrice");
+
+		//Prints the first, second and third item with space using /t and /n fpr new line
+		System.out.println("Burger\t\t\t5\t\t\t\t50php");
+		System.out.println("Fries\t\t\t10\t\t\t\t35php");
+		System.out.println("Coke\t\t\t2\t\t\t\t20php");
+		System.out.println("\nTotal:\t\t\t\t\t\t\t640php");
+		System.out.println("\nThank You For Buying! Please Come Again!");
+
+
+
+	}
+}
