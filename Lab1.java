77e3f730b55bda10fbea64f60b1c21c6ea52fd7f
HannahCeblano <ceblanoh@gmail.com>
2024-10-12 12:45:43 +0800

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -11,13 +11,13 @@ public class Lab1 {
 		System.out.println("Yuki's Store\nOutlook Drive, Baguio City\n");
 
 		//Prints the item name,quantities and price
-		System.out.println("Item name\tQuantities\t\t\tPrice");
+		System.out.println("Item name\tQuantities\t\tPrice");
 
 		//Prints the first, second and third item with space using /t and /n fpr new line
-		System.out.println("Burger\t\t\t5\t\t\t\t50php");
-		System.out.println("Fries\t\t\t10\t\t\t\t35php");
-		System.out.println("Coke\t\t\t2\t\t\t\t20php");
-		System.out.println("\nTotal:\t\t\t\t\t\t\t640php");
+		System.out.println("Burger\t\t5\t\t\t50php");
+		System.out.println("Fries\t\t10\t\t\t35php");
+		System.out.println("Coke\t\t2\t\t\t20php");
+		System.out.println("\nTotal:\t\t\t\t\t640php");
 		System.out.println("\nThank You For Buying! Please Come Again!");
 
 
